ee7e438858d324f473969850d224deac82d17218
SUJAY GOWDA <sujay.gowda@i4sight.net>
2023-12-14 12:56:39 +0530

    Context API & Custom context API

---
+++ src/PostContextProvider.js
@@ -0,0 +1,63 @@
+import { createContext, useState } from 'react';
+import { faker } from '@faker-js/faker';
+import { useContext } from 'react';
+
+// 1. Creating a Context and storing the component in a variable
+
+const PostContext = createContext();
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+export default function PostContextProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 5 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState('');
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onClearPosts: handleClearPosts,
+        onAddPost: handleAddPost,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+function usePosts() {
+  const context = useContext(PostContext);
+  if (context === undefined)
+    console.error('PostContext was used outside of the PostProvider');
+
+  return context;
+}
+
+export { usePosts, PostContextProvider };
